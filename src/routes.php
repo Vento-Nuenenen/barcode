a13b091f74e37508ae9249f3ef872d671ece380b
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-02-12 00:08:26 +0100

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 /*
   |--------------------------------------------------------------------------
-  | Milon/Barcode Routes
+  | Vento/Barcode Routes
   |--------------------------------------------------------------------------
   |
   | Here is where you can register all of the routes for an application.
